74213c7da557c8b4c1165f183c6737f61c1c5a57
srinivaspr <srinivasapavankumarreddy@gmail.com>
2015-04-26 13:05:23 -0700

    client server communication

--- src/main/java/com/ltrf/LogClient.java
+++ src/main/java/com/ltrf/LogClient.java
@@ -1,9 +1,38 @@
 package com.ltrf;
 
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
 public class LogClient {
 	
-	public static void checkTrack(){
-		System.out.println("tracking");
+	private static String uri;
+	
+	public static void trackMessage(LogMessage message) {
+		try{
+			JSONObject request = new JSONObject();
+			request.put("appName", message.getAppName());
+			request.put("key", message.getKey());
+			request.put("message", message.getMessage());
+			request.put("additionalDetails", message.getAdditionalDetails());
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+
+			HttpEntity<String> entity = new HttpEntity<String>(request.toString(),headers);
+			restTemplate.exchange(uri+"/saveLogMessage", HttpMethod.POST, entity, String.class);
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	public static void setURI(String url)
+	{
+		uri = url;
 	}
 
 }
